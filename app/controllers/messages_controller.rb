292cda05a6b4b62f2c8115f1e5e082ddd3b84f2f
Amanuel Galema <amexabee@yahoo.com>
2023-04-01 14:29:58 +0300

    Fix linters

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # Controller for managing messages.
 class MessagesController < ApplicationController
   def index
--- db/migrate/20230401090249_create_messages.rb
+++ db/migrate/20230401090249_create_messages.rb
@@ -1,6 +1,3 @@
-# frozen_string_literal: true
-
-# Migratiton to create messages table.
 class CreateMessages < ActiveRecord::Migration[7.0]
   def change
     create_table :messages do |t|
